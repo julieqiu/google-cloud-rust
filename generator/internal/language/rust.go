2418f25265fdd29d73725251e8d545102a3cdee1
Julie Qiu <julie@golang.org>
2024-11-28 22:01:58 -0500

    impl(internal): split up package genclient
    Split the genclient package, as it contains functions that make more
    sense in other existing packages.

--- generator/internal/language/rust.go
+++ generator/internal/language/rust.go
@@ -25,11 +25,10 @@ import (
 	"unicode"
 
 	"github.com/googleapis/google-cloud-rust/generator/internal/api"
-	"github.com/googleapis/google-cloud-rust/generator/internal/genclient"
 	"github.com/iancoleman/strcase"
 )
 
-func NewRustCodec(copts *genclient.CodecOptions) (*RustCodec, error) {
+func NewRustCodec(copts *CodecOptions) (*RustCodec, error) {
 	year, _, _ := time.Now().Date()
 	codec := &RustCodec{
 		GenerationYear:           fmt.Sprintf("%04d", year),
@@ -798,12 +797,4 @@ func rustEscapeKeyword(symbol string) string {
 		return symbol
 	}
 	return "r#" + symbol
-}
-
-func mapSlice[T, R any](s []T, f func(T) R) []R {
-	r := make([]R, len(s))
-	for i, v := range s {
-		r[i] = f(v)
-	}
-	return r
 }
